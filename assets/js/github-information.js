1d96c8d33cae14446351ec0ad41be4dde4b30bff
ThijsTerporten <twpterporten@gmail.com>
2021-08-09 15:19:12 +0000

    Fix bug for 403 error

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -65,7 +65,11 @@ function fetchGitHubInformation(event) {
         function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No Info Found for user</h2>`)
-            } else {
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-reset')*1000);
+                $("#gh-user-data").html(`<h4>Too Many Requests, please wait untill ${resetTime.toLocaleTimeString}</h4>`)
+            }
+            else {
                 console.log(errorResponse);
                 $("gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
@@ -73,3 +77,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
